4c294e5d7ba372a4e716ce0f4c4c2083646507c2
kingdavelcu <aioflcu@gmail.com>
2026-05-22 23:17:08 +0100

    Fix main.tsx to use proper TanStack Router API with RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 
 const router = getRouter();
 
 const rootElement = document.getElementById("root");
-if (!rootElement?.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement!);
+
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <router.Provider>
-        <router.RootRoute.Component />
-      </router.Provider>
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
